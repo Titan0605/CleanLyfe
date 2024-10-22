ec630d2093787228ba400180cfeb2cf663f6b4fb
Titan0605 <titancisneros@gmail.com>
2024-10-21 20:50:45 -0600

    Responsive base.html

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,4 +1,4 @@
-const btn_nav_sidebar = document.querySelector('.--button_navbar'); /* Function to call a block of html with a class */
+const btn_nav_sidebar = document.querySelector('.navbar__sidebar-button'); /* Function to call a block of html with a class */
 
 btn_nav_sidebar.addEventListener('click', function () { /*Funtion to modify the class of the block selected when is clicked*/
   document.getElementById('sidebar').classList.toggle('active'); /*This specify which class we are going to add or delete*/

8aef8b857f7dd4036e526bc91d94a830883c73d7
Titan0605 <titancisneros@gmail.com>
2024-10-21 22:42:25 -0600

    Adding User options panel

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -9,3 +9,9 @@ const btnsidebar = document.querySelector('.--sidebar');
 btnsidebar.addEventListener('click', function () { /*Funtion to modify the class of the block selected when is clicked*/
   document.getElementById('sidebar').classList.toggle('active'); /*This specify which class we are going to add or delete*/
 });
+
+const btn_user_options = document.querySelector('.user-icon'); /* Function to call a block of html with a class */
+
+btn_user_options.addEventListener('click', function () { /*Funtion to modify the class of the block selected when is clicked*/
+  document.getElementById('user-options').classList.toggle('active'); /*This specify which class we are going to add or delete*/
+});

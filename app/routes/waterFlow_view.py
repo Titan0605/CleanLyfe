313179ec4e3abd0cbdc519a5100f1b329233dccf
Edwihn <a6523150063@utch.edu.mx>
2025-05-14 12:46:16 -0600

    Add route WorldMatters and HTML

---
+++ app/routes/waterFlow_view.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+bp = Blueprint("WaterFlow_routes", __name__)
+
+@bp.route('/waterflow')
+def waterFlow_service():
+    return render_template('waterflow_index.html')

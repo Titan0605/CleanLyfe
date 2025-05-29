6d96c99a99ca715914512ef82893dda37de62c11
Titan0605 <titancisneros@gmail.com>
2025-05-29 00:38:54 -0600

    - Fix index's route reference

--- app/routes/index_views.py
+++ app/routes/index_views.py
@@ -2,6 +2,6 @@ from flask import render_template, Blueprint, url_for
 
 bp = Blueprint("index_views", __name__)
 
-@bp.route('/index_views')
+@bp.route('/')
 def index_views():
     return render_template("index_worldmatters.html")

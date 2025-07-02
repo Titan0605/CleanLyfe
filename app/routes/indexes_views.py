370d2cdbc695fab094cc04ce5ccc6ff6f10011a6
Curlierdig <abdelzr16@gmail.com>
2025-07-01 18:45:03 -0600

    - Sign up responsive

--- app/routes/indexes_views.py
+++ app/routes/indexes_views.py
@@ -2,6 +2,6 @@ from flask import render_template, Blueprint, url_for
 
 bp = Blueprint("indexes_views", __name__)
 
-@bp.route('/index_views')
+@bp.route('/')
 def index_views():
     return render_template("index_worldmatters.html")

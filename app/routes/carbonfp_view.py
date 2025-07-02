10dcbca09b0d3386fc1afc565908e6591e091fcd
Curlierdig <abdelzr16@gmail.com>
2025-07-01 20:08:46 -0600

    -Products responsive
    also the design was updated

--- app/routes/carbonfp_view.py
+++ app/routes/carbonfp_view.py
@@ -5,41 +5,41 @@ bp = Blueprint("carbonfp_view", __name__)
 
 @bp.route('/carbonfp')
 def carbonfp():
-    if 'username' in session:
+    #if 'username' in session:
         return render_template('carbonfp.html')
-    else:
-        return redirect(url_for('index_view.index'))
+    #else:
+     #   return redirect(url_for('index_view.index'))
 
 
 @bp.route('/carbonfp/transport')
 def carbonfp_transport():
-    if 'username' in session:
+    #if 'username' in session:
         return render_template('carbonfp_transport.html')
-    else:
-        return redirect(url_for('index_view.index'))
+    #else:
+     #   return redirect(url_for('index_view.index'))
 
 
 @bp.route('/carbonfp/devices')
 def carbonfp_devices():
-    if 'username' in session:
-        energy_model = ElectricDevicesModel()
+    #if 'username' in session:  
+    energy_model = ElectricDevicesModel()
 
-        devices = energy_model.get_all_devices()
-        return render_template('carbonfp_energy.html', devices=devices)
-    else:
-        return redirect(url_for('index_view.index'))
+    devices = energy_model.get_all_devices()
+    return render_template('carbonfp_energy.html', devices=devices)
+    #else:
+   #     return redirect(url_for('index_view.index'))
 
 
 @bp.route('/carbonfp/products')
 def carbonfp_products():
-    if 'username' in session:
+    #if 'username' in session:
         return render_template('carbonfp_products.html')
-    else:
-        return redirect(url_for('index_view.index'))
+    #else:
+    #    return redirect(url_for('index_view.index'))
     
 @bp.route('/carbonfp/water')
 def carbonfp_water():
-    if 'username' in session:
+    #if 'username' in session:
         return render_template('carbonfp_water.html')
-    else:
-        return redirect(url_for('index_view.index'))
+    #else:
+    #    return redirect(url_for('index_view.index'))
